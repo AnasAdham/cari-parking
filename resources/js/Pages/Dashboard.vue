5e6e72317c71ad8f31310f5d31aaa025c570bf77
Anas Adham <adhamanas24051999@gmail.com>
2021-10-21 22:18:54 +0800

    Campur

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,33 +1,52 @@
 <template>
-    <Head title="Dashboard" />
+  <Head title="Dashboard" />
 
-    <BreezeAuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        You're logged in!
-                    </div>
-                </div>
-            </div>
+  <BreezeAuthenticatedLayout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">Parking View</h2>
+    </template>
+    <div id="parking-lot" class="grid grid-cols-5">
+      <div class="flex justify-center">
+        <div class="my-auto h-96 w-32 bg-blue-100"></div>
+      </div>
+      <div class="col-span-3">
+        <div class="h-3/6 grid grid-cols-5 container mx-auto bg-gray-300 p-9">
+          <div class="parking-spot"></div>
+          <div class="parking-spot"></div>
+          <div class="parking-spot"></div>
+          <div class="parking-spot"></div>
+          <div class="parking-spot"></div>
         </div>
-    </BreezeAuthenticatedLayout>
+        <div class="h-3/6 grid grid-cols-5 container mx-auto bg-gray-300 p-9">
+          <div class="parking-spot"></div>
+          <div class="parking-spot"></div>
+          <div class="parking-spot"></div>
+          <div class="parking-spot"></div>
+          <div class="parking-spot"></div>
+        </div>
+      </div>
+      <div class="flex justify-center">
+        <div class="my-auto h-96 w-32 bg-blue-100"></div>
+      </div>
+    </div>
+  </BreezeAuthenticatedLayout>
 </template>
 
 <script>
-import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
-import { Head } from '@inertiajs/inertia-vue3';
+import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
+import { Head } from "@inertiajs/inertia-vue3";
+import Button from "@/Components/Button.vue";
 
 export default {
-    components: {
-        BreezeAuthenticatedLayout,
-        Head,
-    },
-}
+  components: {
+    BreezeAuthenticatedLayout,
+    Button,
+    Head,
+  },
+};
 </script>
+<style scoped>
+#parking-lot {
+  height: 560px;
+}
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import "tailwindcss/tailwind.css";
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
